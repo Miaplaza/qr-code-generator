77fac2adfac86fd82612452c9a5d72f7507724ae
Andrii Achilov <aachilov@mia.school>
2024-05-16 02:17:47 +0300

    Add validation of URL length; minor code refactoring.

--- src/components/QrCode.vue
+++ src/components/QrCode.vue
@@ -5,29 +5,33 @@
         <h1>QR code generator</h1>
       </div>
     </div>
-    <form ref="qrCodeForm" class="row justify-content-center" novalidate :inert="isQRCodeGenerated">
+    <form ref="generateQRCodeForm" class="row justify-content-center" novalidate>
       <div class="col-auto has-validation url-input">
-        <input type="url" placeholder="Enter URL link" v-model="urlToGenerate" class="form-control" required>
+        <input ref="urlInput" v-model="urlToGenerate" type="url" placeholder="Enter URL link" minlength="25" required
+          :readonly="isQRCodeGenerated" class="form-control">
         <div class="invalid-feedback">
           Please enter a correct URL link.
+          <br>
+          Also, make sure your URL contains at least 25 symbols and no more than 280 symbols
+          in order to create a scannable QR code.
+          You can add whitespaces at the end if the link is too short.
         </div>
       </div>
       <div class="col-auto">
-        <button class="btn btn-primary" @click.prevent="generateQRCode(urlToGenerate)"
-          :disabled="isQRCodeGenerated">
+        <button class="btn btn-primary" :disabled="isQRCodeGenerated" @click.prevent="generateQrCode(urlToGenerate)">
           Generate
         </button>
       </div>
     </form>
     <div class="row justify-content-center my-3">
       <div class="col-auto">
-        <div class="qr-code" ref="qrCodeComponent"></div>
+        <div ref="qrCodeComponent" class="qr-code"></div>
       </div>
     </div>
     <template v-if="isQRCodeGenerated">
       <form class="row justify-content-center">
         <div class="col-auto">
-          <select class="form-select" v-model="extension">
+          <select v-model="extension" class="form-select">
             <option value="svg">SVG</option>
             <option selected value="png">PNG</option>
             <option value="jpeg">JPEG</option>
@@ -35,13 +39,16 @@
           </select>
         </div>
         <div class="col-auto">
-          <button class="btn btn-primary" @click.prevent="download('qrCode', extension)">Download</button>
+          <button class="btn btn-primary" @click.prevent="download('qrCode', extension)">
+            Download
+          </button>
         </div>
       </form>
       <div class="row justify-content-center mt-5">
         <div class="col-auto">
-          <button class="btn btn-lg btn-success" @click.prevent="reset()" :disabled="!isQRCodeGenerated">Generate
-            new QR code</button>
+          <button class="btn btn-lg btn-success" :disabled="!isQRCodeGenerated" @click.prevent="reset()">
+            Generate new QR code
+          </button>
         </div>
       </div>
     </template>
@@ -57,12 +64,14 @@ import QRCodeStyling, {
 
 const blackColor = "#000000";
 const whiteColor = "#ffffff";
+const maxUrlLength = 280;
+
+const generateQRCodeForm = ref<HTMLFormElement>(null!);
+const qrCodeComponent = ref<HTMLElement>(null!);
+const urlInput = ref<HTMLInputElement>(null!);
 
 const isQRCodeGenerated = ref(false);
 const urlToGenerate = ref("");
-
-const qrCodeForm = ref<HTMLFormElement>(null!);
-const qrCodeComponent = ref<HTMLElement>(null!);
 const extension = ref<FileExtension>('png');
 const options = ref<Options>({
   width: 300,
@@ -99,11 +108,8 @@ const options = ref<Options>({
 });
 var qrCode = new QRCodeStyling(options.value);
 
-function generateQRCode(url: string): void {
-  if (!qrCodeForm.value.checkValidity()) {
-    isQRCodeGenerated.value = false;
-    qrCodeForm.value.classList.add('was-validated');
-    qrCodeComponent.value.replaceChildren();
+function generateQrCode(url: string): void {
+  if (!validateQrCodeGenerationForm()) {
     return;
   }
 
@@ -112,11 +118,30 @@ function generateQRCode(url: string): void {
   isQRCodeGenerated.value = true;
 }
 
+function validateQrCodeGenerationForm(): boolean {
+  urlInput.value.setCustomValidity('');
+
+  if (!generateQRCodeForm.value.checkValidity()) {
+    generateQRCodeForm.value.classList.add('was-validated');
+    return false;
+  }
+  // Makes max length limitation soft, without cutoff (unlike 'maxlength' attribute).
+  if (urlInput.value.value.length > maxUrlLength) {
+    // Triggers invalid state of the URL input.
+    // The error message doesn't matter because we will show predefined error message.
+    urlInput.value.setCustomValidity('error');
+    generateQRCodeForm.value.classList.add('was-validated');
+    return false;
+  }
+
+  return true;
+}
+
 function reset(): void {
   isQRCodeGenerated.value = false;
-  qrCodeForm.value.classList.remove('was-validated');
-  qrCodeComponent.value.replaceChildren();
   urlToGenerate.value = "";
+  generateQRCodeForm.value.classList.remove('was-validated');
+  qrCodeComponent.value.replaceChildren();
 }
 
 async function download(name: string, extension: FileExtension): Promise<void> {
