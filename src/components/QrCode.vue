e9157351a93deee33c6010f168bfdde87f29cec4
Andrii Achilov <aachilov@mia.school>
2024-05-16 02:14:14 +0300

    Add validation; simplify HTML structure

--- src/components/QrCode.vue
+++ src/components/QrCode.vue
@@ -1,39 +1,50 @@
 <template>
-  <main>
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-auto">
-          <form class="row justify-content-center">
-            <div class="col-auto">
-              <input type="url" placeholder="Enter URL link" v-model="urlToGenerate" class="form-control url-input">
-            </div>
-            <div class="col-auto">
-              <button class="btn btn-primary" @click.prevent="generateQRCode(urlToGenerate)">Generate</button>
-            </div>
-          </form>
-          <div class="row justify-content-center">
-            <div class="col-auto my-3">
-              <div class="qr-code" ref="qrCodeComponent"></div>
-            </div>
-          </div>
-          <template v-if="isQRCodeGenerated">
-            <div class="row justify-content-center">
-              <div class="col-auto">
-                <select class="form-select" v-model="extension">
-                  <option value="svg">SVG</option>
-                  <option selected value="png">PNG</option>
-                  <option value="jpeg">JPEG</option>
-                  <option value="webp">WEBP</option>
-                </select>
-              </div>
-              <div class="col-auto">
-                <button class="btn btn-primary" @click.prevent="download('qrCode', extension)">Download</button>
-              </div>
-            </div>
-          </template>
-        </div>
+  <main class="container">
+    <div class="row justify-content-center my-4">
+      <div class="col-auto">
+        <h1>QR code generator</h1>
       </div>
     </div>
+    <form ref="qrCodeForm" class="row justify-content-center" novalidate :inert="isQRCodeGenerated">
+      <div class="col-auto has-validation url-input">
+        <input type="url" placeholder="Enter URL link" v-model="urlToGenerate" class="form-control" required>
+        <div class="invalid-feedback">
+          Please enter a correct URL link.
+        </div>
+      </div>
+      <div class="col-auto">
+        <button class="btn btn-primary" @click.prevent="generateQRCode(urlToGenerate)"
+          :disabled="isQRCodeGenerated">
+          Generate
+        </button>
+      </div>
+    </form>
+    <div class="row justify-content-center my-3">
+      <div class="col-auto">
+        <div class="qr-code" ref="qrCodeComponent"></div>
+      </div>
+    </div>
+    <template v-if="isQRCodeGenerated">
+      <form class="row justify-content-center">
+        <div class="col-auto">
+          <select class="form-select" v-model="extension">
+            <option value="svg">SVG</option>
+            <option selected value="png">PNG</option>
+            <option value="jpeg">JPEG</option>
+            <option value="webp">WEBP</option>
+          </select>
+        </div>
+        <div class="col-auto">
+          <button class="btn btn-primary" @click.prevent="download('qrCode', extension)">Download</button>
+        </div>
+      </form>
+      <div class="row justify-content-center mt-5">
+        <div class="col-auto">
+          <button class="btn btn-lg btn-success" @click.prevent="reset()" :disabled="!isQRCodeGenerated">Generate
+            new QR code</button>
+        </div>
+      </div>
+    </template>
   </main>
 </template>
 
@@ -50,6 +61,7 @@ const whiteColor = "#ffffff";
 const isQRCodeGenerated = ref(false);
 const urlToGenerate = ref("");
 
+const qrCodeForm = ref<HTMLFormElement>(null!);
 const qrCodeComponent = ref<HTMLElement>(null!);
 const extension = ref<FileExtension>('png');
 const options = ref<Options>({
@@ -87,14 +99,28 @@ const options = ref<Options>({
 });
 var qrCode = new QRCodeStyling(options.value);
 
-function generateQRCode(url: string) {
+function generateQRCode(url: string): void {
+  if (!qrCodeForm.value.checkValidity()) {
+    isQRCodeGenerated.value = false;
+    qrCodeForm.value.classList.add('was-validated');
+    qrCodeComponent.value.replaceChildren();
+    return;
+  }
+
   qrCode.update({ data: url });
   qrCode.append(qrCodeComponent.value);
   isQRCodeGenerated.value = true;
 }
 
-function download(name: string, extension: FileExtension) {
-  qrCode.download({ name: name, extension: extension });
+function reset(): void {
+  isQRCodeGenerated.value = false;
+  qrCodeForm.value.classList.remove('was-validated');
+  qrCodeComponent.value.replaceChildren();
+  urlToGenerate.value = "";
+}
+
+async function download(name: string, extension: FileExtension): Promise<void> {
+  await qrCode.download({ name: name, extension: extension });
 }
 </script>
 
--- src/components/TheNavbar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <header class="container my-4">
-    <div class="row justify-content-center">
-      <div class="col-auto">
-        <h1>QR code generator</h1>
-      </div>
-    </div>
-  </header>
-</template>
